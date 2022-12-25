e9a74fbd2c6188ed035c355f4026c841dbbd70a0
bszeidl <szeidl.balazs@hotmail.com>
2022-12-25 18:31:58 +0100

    Many triangles are ready.

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
 function pageLoaded () {
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
-    context.fillRect(175,100,100,100);  
+    
+    let origoX = 0;
+    let origoY = canvas.height * .5;
+
+    let peakX = canvas.clientWidth;
+    let peakY = 0;
+
+    
+
+    for (i = 0; i < canvas.width; i++) {
+
+        if (origoX % 4 === 0) {
+
+            peakX = 0;
+            peakY = 0;
+
+        } else if (origoX % 4 === 1) {
+
+            peakX = canvas.width;
+            peakY = 0;
+
+        } else if (origoX % 4 === 2) {
+
+            peakX = 0;
+            peakY = canvas.height;
+
+        } else if (origoX % 4 === 3) {
+
+            peakX = canvas.width;
+            peakY = canvas.height;
+            
+        }
+
+        context.beginPath();
+
+        context.moveTo(origoX,origoY);
+        context.lineTo(peakX,peakY);
+        context.strokeStyle = 'rgba(0,0,255,.5)';
+        context.stroke();
+
+        origoX += 1;
+        
+        
+    }
+
+    
 
 }
 
